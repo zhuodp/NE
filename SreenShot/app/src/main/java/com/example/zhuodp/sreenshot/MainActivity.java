e930302dd5434cb142671ad99dd0413c188aedcc
unknown <zhuodp@youdao.internal>
2018-12-13 18:11:45 +0800

    1.完善了针对单个滑动View的滚动截屏的实现  2. 取缓存的截屏方式存在NullPointer问题(易解决) 3.带状态栏截屏和滚动截屏接口待提取  4.Date:2018/12/13

--- SreenShot/app/src/main/java/com/example/zhuodp/sreenshot/MainActivity.java
+++ SreenShot/app/src/main/java/com/example/zhuodp/sreenshot/MainActivity.java
@@ -12,64 +12,44 @@ package com.example.zhuodp.sreenshot;
 
  */
 
-import android.Manifest;
-import android.annotation.SuppressLint;
 import android.annotation.TargetApi;
 import android.app.Activity;
 import android.content.Context;
 import android.content.Intent;
-import android.content.pm.PackageManager;
 import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Paint;
 import android.graphics.Picture;
-import android.graphics.PixelFormat;
 import android.graphics.drawable.ColorDrawable;
 import android.graphics.drawable.Drawable;
-import android.hardware.display.DisplayManager;
-import android.hardware.display.VirtualDisplay;
-import android.media.Image;
-import android.media.ImageReader;
-import android.media.projection.MediaProjection;
 import android.media.projection.MediaProjectionManager;
-import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
-import android.os.Handler;
-import android.support.v4.app.ActivityCompat;
-import android.support.v4.content.ContextCompat;
-import android.support.v7.app.AppCompatActivity;
+import android.os.SystemClock;
 import android.os.Bundle;
 import android.support.v7.widget.RecyclerView;
-import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.LruCache;
-import android.view.GestureDetector;
-import android.view.LayoutInflater;
-import android.view.Surface;
+import android.view.MotionEvent;
 import android.view.View;
+import android.view.ViewConfiguration;
 import android.view.ViewGroup;
 import android.view.ViewTreeObserver;
-import android.view.WindowManager;
 import android.webkit.WebView;
 import android.widget.ArrayAdapter;
-import android.widget.BaseAdapter;
 import android.widget.Button;
 import android.widget.ImageView;
 import android.widget.LinearLayout;
 import android.widget.ListAdapter;
 import android.widget.ListView;
 import android.widget.RelativeLayout;
-import android.widget.TextView;
 import android.widget.Toast;
 
 import com.example.zhuodp.sreenshot.ScreenShotUtils.ScreenShotHelper;
 import com.example.zhuodp.sreenshot.ScreenShotUtils.ShotApplication;
-import com.pgssoft.scrollscreenshot.ScrollScreenShot;
 
 import java.io.File;
 import java.io.FileOutputStream;
-import java.nio.ByteBuffer;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -81,9 +61,14 @@ public class MainActivity extends Activity {
     private Button btnLinearLayoutScreenShot;
     private Button btnListViewScreenShot;
     private Button btnWholeScreenScreenShot;
+    private Button btnLongScreenScreenShot;
+    private Button btnSingleViewScreenShot;
+    private Button btnOutOfBoundsTest;
+    private boolean longScreenShotControlFlag;
     public static ImageView imgScreenShotResult;
     private ListView listView;
     private LinearLayout linearLayout;
+
 
     private int result = 0;
     private Intent intent = null;
@@ -98,7 +83,6 @@ public class MainActivity extends Activity {
         setContentView(R.layout.activity_main);
         findView();
         initData();
-        ScreenShotHelper screenShotHelper = new ScreenShotHelper();
         setListener();
     }
 
@@ -109,13 +93,17 @@ public class MainActivity extends Activity {
         btnLinearLayoutScreenShot =(Button)findViewById(R.id.btn_linearLayoutScreenShot);
         btnListViewScreenShot =(Button)findViewById(R.id.btn_listViewSreenShot);
         btnWholeScreenScreenShot =(Button)findViewById(R.id.btn_wholeScreenScreenShot);
+        btnLongScreenScreenShot =(Button)findViewById(R.id.btn_longScreenScreenShot);
+        btnSingleViewScreenShot =(Button)findViewById(R.id.btn_singleViewScreenShot);
+        btnOutOfBoundsTest =(Button)findViewById(R.id.btn_outOfBoundsTest);
 
         imgScreenShotResult =(ImageView)findViewById(R.id.id_screenShotResult);
         listView = (ListView)findViewById(R.id.listView);
+        container = (LinearLayout)findViewById(R.id.linearLayout);
 
     }
     private void initData(){
-        for (int i =1;i<=20;i++){
+        for (int i =1;i<=25;i++){
             listViewData.add(i);
         }
         listView= (ListView) findViewById(R.id.listView);
@@ -132,7 +120,6 @@ public class MainActivity extends Activity {
         btnNormalSreenShot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-
                 //方案1：普通截屏
                 imgScreenShotResult.setImageBitmap(screenShotNormalScreen());
                 //方案1衍生：截取View
@@ -151,6 +138,7 @@ public class MainActivity extends Activity {
             }
         });
 
+        //针对ListView
         btnListViewScreenShot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -163,16 +151,46 @@ public class MainActivity extends Activity {
             }
         });
 
+        //针对全屏
         btnWholeScreenScreenShot.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
                 Log.d("zhuodp","整屏截图");
                 Bitmap bitmap = screenShotHelper.sreenShotOnWholeScreen(getApplicationContext());
-
                 screenShotHelper.setUpMediaProjection(getApplicationContext());
                 //这里需要考虑时序
             }
         });
+
+
+        //滚动截取长屏
+        btnLongScreenScreenShot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if(longScreenShotControlFlag){
+                    bitmaps.clear();
+                    imgScreenShotResult.setImageBitmap(null);
+                    isScreenShots = true;
+                    btnLongScreenScreenShot.setBackgroundColor(getResources().getColor(R.color.red));
+                    longScreenShotControlFlag = false;
+                    autoScroll();
+                }else{
+                    isScreenShots = false;
+                    longScreenShotControlFlag = true;
+                    btnLongScreenScreenShot.setBackgroundColor(getResources().getColor(R.color.green));
+                }
+            }
+        });
+
+        //针对单个View
+        btnSingleViewScreenShot.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                imgScreenShotResult.setImageBitmap(screenShotHelper.screenShotOnSingleView(btnSingleViewScreenShot));
+            }
+        });
+
+
 
     }
 
@@ -439,4 +457,141 @@ public class MainActivity extends Activity {
      * which is packaged with this application.
      */
     public native String stringFromJNI();
+
+
+
+
+
+
+
+
+
+
+
+    //长截屏
+    private boolean isScreenShots = false;
+    private ViewGroup container;
+    private List<Bitmap> bitmaps = new ArrayList<>();
+
+    private void autoScroll() {
+        final int delay = 16;//延时16毫秒分发滑动事件
+        final int step = 10;//每次滑动距离5像素，可以根据需要调整（若卡顿的话实际滚动距离可能小于5）
+        final MotionEvent motionEvent = MotionEvent.obtain(SystemClock.uptimeMillis()
+                , SystemClock.uptimeMillis()
+                , MotionEvent.ACTION_DOWN
+                , listView.getWidth() / 2
+                , listView.getHeight() / 2
+                , 0);
+        //先分发 MotionEvent.ACTION_DOWN 事件，我们指定为按下位置是listview的中间位置，当然其他位置也可以
+        //
+        //View mView = getWindow().getDecorView();
+        listView.dispatchTouchEvent(motionEvent);
+        /*
+        注意：
+        查看Listview源码可知 滑动距离大于ViewConfiguration.get(view.getContext()).getScaledTouchSlop()时listview才开始滚动
+         private boolean startScrollIfNeeded(int x, int y, MotionEvent vtev) {
+            // Check if we have moved far enough that it looks more like a
+            // scroll than a tap
+            final int deltaY = y - mMotionY;
+            final int distance = Math.abs(deltaY);
+            final boolean overscroll = mScrollY != 0;
+            if ((overscroll || distance > mTouchSlop) && (getNestedScrollAxes() & SCROLL_AXIS_VERTICAL) == 0) {
+                ...
+                return true;
+            }
+            return false;
+        }
+         */
+        motionEvent.setAction(MotionEvent.ACTION_MOVE);
+        motionEvent.setLocation(motionEvent.getX(), motionEvent.getY() - (ViewConfiguration.get(listView.getContext()).getScaledTouchSlop()));
+        listView.dispatchTouchEvent(motionEvent);
+
+        final int startScrollY = (int) motionEvent.getY();
+
+        listView.postDelayed(new Runnable() {
+            @Override
+            public void run() {
+                if (isScreenShots == false) {//注意：我们无法通过滚动距离来判断是否滚动到了最后，所以需要通过其他方式停止滚动
+                    drawRemainAndAssemble(startScrollY, (int) motionEvent.getY());
+                    return;
+                }
+                //滚动刚好一整屏时画到bitmap上
+                drawIfNeeded(startScrollY, (int) motionEvent.getY());
+
+                motionEvent.setAction(MotionEvent.ACTION_MOVE); //延时分发 MotionEvent.ACTION_MOVE 事件
+                /*
+                  改变motionEvent的y坐标，nextStep越大滚动越快，但太大可能会导致掉帧，导致实际滚动距离小于我们滑动的距离
+
+                  因为我们是根据(curScrollY - startScrollY) % container.getHeight() == 0来判定是否刚好滚动了一屏幕的，
+                  所以快要滚动到刚好一屏幕位置时，修改nextStep的值，使下次滚动后刚好是一屏幕的距离。
+                  当然nextStep也可以一直是1，这时就不需要凑整了，但这样会导致滚动的特别慢
+                 */
+                int nextStep;
+                int gap = (startScrollY - (int) motionEvent.getY() + step) % container.getHeight();
+                if (gap > 0 && gap < step) {
+                    nextStep = step - gap;
+                } else {
+                    nextStep = step;
+                }
+
+                motionEvent.setLocation((int) motionEvent.getX(), (int) motionEvent.getY() - nextStep);
+                listView.dispatchTouchEvent(motionEvent);
+
+                listView.postDelayed(this, delay);
+            }
+        }, delay);
+    }
+
+    private void drawRemainAndAssemble(int startScrollY, int curScrollY) {
+        //最后的可能不足一屏幕，需要单独处理
+        if ((curScrollY - startScrollY) % container.getHeight() != 0) {
+            Bitmap film = Bitmap.createBitmap(container.getWidth(), container.getHeight(), Bitmap.Config.RGB_565);
+            Canvas canvas = new Canvas();
+            canvas.setBitmap(film);
+            container.draw(canvas);
+
+            int part = (startScrollY - curScrollY) / container.getHeight();
+            int remainHeight = startScrollY - curScrollY - container.getHeight() * part;
+            Bitmap remainBmp = Bitmap.createBitmap(film, 0, container.getHeight() - remainHeight, container.getWidth(), remainHeight);
+            bitmaps.add(remainBmp);
+        }
+
+        assembleBmp();
+
+    }
+
+    private void assembleBmp() {
+        int h = 0;
+        for (Bitmap bitmap : bitmaps) {
+            h += bitmap.getHeight();
+        }
+        Bitmap bitmap = Bitmap.createBitmap(container.getWidth(), h, Bitmap.Config.RGB_565);
+        Canvas canvas = new Canvas(bitmap);
+        for (Bitmap b : bitmaps) {
+            canvas.drawBitmap(b, 0, 0, null);
+            canvas.translate(0, b.getHeight());
+        }
+        ViewGroup.LayoutParams params = imgScreenShotResult.getLayoutParams();
+        params.width = bitmap.getWidth() * 2;
+        params.height = bitmap.getHeight() * 2;
+        imgScreenShotResult.requestLayout();
+        imgScreenShotResult.setImageBitmap(bitmap);
+    }
+
+    private void drawIfNeeded(int startScrollY, int curScrollY) {
+
+        if ((curScrollY - startScrollY) % container.getHeight() == 0) {
+            //正好滚动满一屏
+
+            //为了更通用，我们是把ListView的父布局（和ListView宽高相同）画到了bitmap上
+
+            Bitmap film = Bitmap.createBitmap(container.getWidth(), container.getHeight(), Bitmap.Config.RGB_565);
+            Canvas canvas = new Canvas();
+            canvas.setBitmap(film);
+            container.draw(canvas);
+            bitmaps.add(film);
+        }
+    }
+
+
 }
--- SreenShot/app/src/main/java/com/example/zhuodp/sreenshot/ScreenShotUtils/ScreenShotHelper.java
+++ SreenShot/app/src/main/java/com/example/zhuodp/sreenshot/ScreenShotUtils/ScreenShotHelper.java
@@ -25,6 +25,7 @@ import android.os.AsyncTask;
 import android.os.Build;
 import android.os.Environment;
 import android.os.Handler;
+import android.os.SystemClock;
 import android.provider.MediaStore;
 import android.support.annotation.RequiresApi;
 import android.support.v7.widget.RecyclerView;
@@ -32,6 +33,7 @@ import android.util.DisplayMetrics;
 import android.util.Log;
 import android.util.LruCache;
 import android.view.LayoutInflater;
+import android.view.MotionEvent;
 import android.view.View;
 import android.view.WindowManager;
 import android.webkit.WebView;
@@ -409,145 +411,8 @@ public class ScreenShotHelper {
 
 
 
+    //长截屏
 
-
-
-
-
-
-   /* //长截屏
-    private int mScreenWidth;
-    private int mScreenHeight;
-    //private int mScreenDensity;
-    // 截图结束的标记
-    private boolean isStopFlag = false;
-    // 第一次截图完成之后的标记
-    private boolean isStop = false;
-    private boolean isRunning = false;
-    private Handler handler = new Handler();
-
-    private void startScreenShot() {
-
-        handler.postDelayed(new Runnable() {
-            public void run() {
-                startCapture();
-            }
-        }, 30);
-    }
-
-    private void createImageReader() {
-        // 设置截屏的宽高
-        mImageReader = ImageReader.newInstance(mScreenWidth, mScreenHeight, PixelFormat.RGBA_8888, 1);
-    }
-    *//**
-     * 最终得到当前屏幕的内容，注意这里mImageReader.getSurface()被传入，屏幕的数据也将会在ImageReader中的Surface中
-     *//*
-    private void virtualDisplay() {
-        mVirtualDisplay = MainActivity.getMediaProjection().createVirtualDisplay("screen-mirror",
-                mScreenWidth, mScreenHeight, mScreenDensity, DisplayManager.VIRTUAL_DISPLAY_FLAG_AUTO_MIRROR,
-                mImageReader.getSurface(), null, null);
-    }
-    private void startCapture() {
-        // 这个方法已经被调用过，在获取另外一个新的image之前，请先关闭原有有的image
-        Image image = mImageReader.acquireLatestImage();
-        if (image == null) {
-            startScreenShot();
-        } else {
-            SaveTask mSaveTask = new SaveTask();
-            mSaveTask.execute(image);
-        }
-    }
-
-    private Bitmap finalImage = null;
-    public class SaveTask extends AsyncTask<Image, Void, Bitmap> {
-
-        @Override
-        protected Bitmap doInBackground(Image... params) {
-            if (params == null || params.length < 1 || params[0] == null) {
-                return null;
-            }
-            Image image = params[0];
-            int width = image.getWidth();
-            int height = image.getHeight();
-            final Image.Plane[] planes = image.getPlanes();
-            final ByteBuffer buffer = planes[0].getBuffer();
-            // 每个像素的间距
-            int pixelStride = planes[0].getPixelStride();
-            // 总的间距
-            int rowStride = planes[0].getRowStride();
-            int rowPadding = rowStride - pixelStride * width;
-            Bitmap bitmap = Bitmap.createBitmap(width + rowPadding / pixelStride, height, Bitmap.Config.ARGB_8888);
-
-            bitmap.copyPixelsFromBuffer(buffer);
-            if (!isStopFlag) {
-                // 截图
-                bitmap = ImageUtils.screenShotBitmap(getApplicationContext(), bitmap, false);
-                if (finalImage == null) {
-                    finalImage = bitmap;
-                }
-                if (finalImage != bitmap) {
-                    finalImage = SewUtils.merge(finalImage, bitmap);
-                }
-            } else {
-                bitmap = ImageUtils.screenShotBitmap(getApplicationContext(), bitmap, true);
-                finalImage = SewUtils.merge(finalImage, bitmap);
-            }
-            bitmap = finalImage;
-            image.close();
-            File fileImage = null;
-            if (bitmap != null) {
-                try {
-                    if (isStopFlag) {
-                        fileImage = new File(FileUtils.getFileName(getApplicationContext()));
-                        FileOutputStream out = new FileOutputStream(fileImage);
-                        bitmap.compress(Bitmap.CompressFormat.PNG, 100, out);
-                        out.flush();
-                        out.close();
-                        Intent media = new Intent(Intent.ACTION_MEDIA_SCANNER_SCAN_FILE);
-                        Uri contentUri = Uri.fromFile(fileImage);
-                        media.setData(contentUri);
-                        sendBroadcast(media);
-                    }
-                } catch (Exception e) {
-                    e.printStackTrace();
-                }
-            }
-            if (fileImage != null) {
-                return bitmap;
-            }
-            return null;
-        }
-
-        @Override
-        protected void onPostExecute(Bitmap bitmap) {
-            super.onPostExecute(bitmap);
-            if (mFloatView.getVisibility() == View.GONE) {
-                mFloatView.setVisibility(View.VISIBLE);
-                isRunning = false;
-                isStopFlag = false;
-
-                finalImage.recycle();
-                finalImage = null;
-
-                new Handler().postDelayed(new Runnable() {
-                    @Override
-                    public void run() {
-                        Intent localIntent = new Intent(FloatWindowsService.this, MainActivity.class);
-                        localIntent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
-                        FloatWindowsService.this.startActivity(localIntent);
-                    }
-                }, 500L);
-            }
-        }
-    }
-
-    private void stopVirtual() {
-        if (mVirtualDisplay == null) {
-            return;
-        }
-        mVirtualDisplay.release();
-        mVirtualDisplay = null;
-    }*/
 
 
 }
